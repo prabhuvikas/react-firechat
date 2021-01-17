9bbc1c9ff02e0c293acbc45e45a9536a93937360
prabhuvikas <vikasprabhu@credenceanalytics.com>
2021-01-17 18:02:04 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// Firebase deps
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAQ3f8k0_1t1LbJJwwAXoM4UUlzxT9snA4",
+  authDomain: "react-firechat-344a1.firebaseapp.com",
+  projectId: "react-firechat-344a1",
+  storageBucket: "react-firechat-344a1.appspot.com",
+  messagingSenderId: "166530061936",
+  appId: "1:166530061936:web:fbde0a35ca589bea15c6b5"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
